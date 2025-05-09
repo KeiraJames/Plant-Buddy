f4197f542c8988144a40d46e160f381b02d81667
IbrahimF1 <106222288+IbrahimF1@users.noreply.github.com>
2025-05-09 18:31:44 -0400

    Update api_config.py

--- api_config.py
+++ api_config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PLANTNET_API_KEY = os.getenv("PLANTNET_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = "AIzaSyCd-6N83gfhMx_-D4WCAc-8iOFSb6hDJ_Q"
 MONGO_URI = os.getenv("MONGO_URI")
